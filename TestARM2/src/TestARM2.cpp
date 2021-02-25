2f02be09869aec57a9592aa05a1e9fc411c4a691
vishnu <vishnu@debian>
2021-02-25 21:26:19 +0530

    first commit

---
+++ TestARM2/src/TestARM2.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : TestARM2.cpp
+// Author      : vishnu ks
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+int x =5 ;
+x++ ;
+	cout << "the value of x is" << x << endl; // prints !!!Hello World!!!
+	return 0;
+}
